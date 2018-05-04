b2280d90bfdb7304e281601f47f4ebf850b92596
cacerescorem <cacerescorem@gmail.com>
2018-05-04 14:08:29 +0200

    cliebtes2

--- routes/cliente.js
+++ routes/cliente.js
@@ -5,6 +5,23 @@ var Cliente = require('../models/cliente.js');
 
 var app = express();
 
+app.get('/', (req, res, next) => {
+
+    Cliente.find({}).exec((err, clientes)=>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error acceso DB',
+                errores: err
+            })
+        }
+        res.status(200).json({
+            ok: true,
+            clientes: clientes
+        })
+    });
+});
+
 app.get('/nombre/:nombre', (req, res, next) => {
 
     var nombre = req.params.nombre;
