bfa3dba2ce9b09a3a3f497f10425d237ba58b135
cacerescorem <cacerescorem@gmail.com>
2018-05-08 12:48:37 +0200

    editpres

--- models/presupuesto.js
+++ models/presupuesto.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 //var unique = require('mongoose-unique-validator');
+var autoIncrement = require('mongoose-auto-increment');
+
+var connection = mongoose.createConnection('mongodb://localhost:27017/erp');
+autoIncrement.initialize(connection);
 
 var PresupuestoSchema = new mongoose.Schema({
     cliente: String,
+    cif: String,
     fecha: String,
     items: Array,
     suma: Number,
@@ -12,5 +17,6 @@ var PresupuestoSchema = new mongoose.Schema({
 })
 
 //PresupuestoSchema.plugin(unique, { message: 'El cif introducido ya existe'});
+PresupuestoSchema.plugin(autoIncrement.plugin, { model: 'Presupuesto', field: 'numero', startAt: 1} );
 
 module.exports = mongoose.model('Presupuesto', PresupuestoSchema);
